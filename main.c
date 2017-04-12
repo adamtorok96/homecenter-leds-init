6e505f930339b9df7c75a9dde5c297279ed8b8db
Adam Torok <adamtorok@sch.bme.hu>
2017-04-12 21:23:39 +0200

    Init

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+static int leds[3] = {12, 13, 14};
+
+int main(int argc, char * args[]) {
+
+    if( wiringPiSetup() == -1 )
+        return 1;
+
+    int i;
+    for(i = 0; i < 3; i++) {
+        softPwmCreate(leds[i], 0, 100);
+        softPwmWrite(leds[i], 0);
+    }
+
+    return 0;
+}
